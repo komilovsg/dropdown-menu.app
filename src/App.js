6e3a674ffeeb11e0eb491752da7dca449292b0e5
komilovsg <komilovsg@gmail.com>
2023-08-31 02:14:24 +0500

    dropdown-menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="App">
-      
+      <div>
+        <button onClick={handleToggle} className="dropdown-toggle">
+          Toggle Dropdown
+        </button>
+        {isOpen && (
+          <ul className="dropdown-menu">
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
